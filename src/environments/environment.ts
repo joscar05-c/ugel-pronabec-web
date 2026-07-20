8be5f7ef7fcb9ddc442a2488ee2d8788f9a47b37
joscar05-c <pizangojosec@gmail.com>
2026-07-20 11:39:07 -0500

    Restore Angular environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://frudtifmyxnzxejaclio.supabase.co/',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydWR0aWZteXhuenhlamFjbGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNzg3ODgsImV4cCI6MjA5OTY1NDc4OH0.hB9vkVhW-tpb3i-EkopKFrjWeFNdP9QhZMgdP4zg564'
+};
+
